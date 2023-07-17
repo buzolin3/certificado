2c43f8b46bcc63073ad4a92853f9b4033ebc0afa
buzolin3 <130096669+buzolin3@users.noreply.github.com>
2023-07-17 11:06:29 -0300

    certificado1.2

---
+++ home.js
@@ -0,0 +1,7 @@
+function menu_burguer() {
+    if (navbar_menu.style.display == 'block' ) {
+        navbar_menu.style.display = 'none'
+    } else {
+        navbar_menu.style.display = 'block'
+    }
+}
